73d453ed2fee90c5562dabbfa6bb1500b5035629
robclaassen99 <83074647+robclaassen99@users.noreply.github.com>
2022-09-19 16:14:31 +0200

    Generated validation instances for run

--- DataGen/generate_data.py
+++ DataGen/generate_data.py
@@ -5,9 +5,10 @@ j = 6
 m = 6
 t = 2
 l = 1
-h = 50
+h = 99
 lt_l = 1
-lt_h = 50
+lt_h = 99
+run_type = 'L2D-LeadTime_Loading'
 batch_size = 100
 seed = 200
 
@@ -19,5 +20,5 @@ t_array = np.array([uni_instance_gen(n_j=j, n_m=m, n_t=t, low=l, high=h, lt_low=
                     for _ in range(batch_size)])
 print(data.shape)
 print(t_array.shape)
-np.save('generatedDataLTTruckVRL{}_{}_{}_Seed{}.npy'.format(j, m, t, seed), data)
-np.save('generatedArrayLTTruckVRL{}_{}_{}_Seed{}.npy'.format(j, m, t, seed), t_array)
+np.save('generatedData_{}_{}_{}_{}_Seed{}.npy'.format(run_type, j, m, t, seed), data)
+np.save('generatedArray_{}_{}_{}_{}_Seed{}.npy'.format(run_type, j, m, t, seed), t_array)
